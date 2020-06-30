9f2f84eae23468481107759868ec651c38302dc8
yanzhenqiang <yanzhenqiang@didiglobal.com>
2020-06-30 20:18:45 +0800

    tmp

--- evolution.py
+++ evolution.py
@@ -31,52 +31,47 @@ import collections
 import itertools
 import multiprocessing
 
-class SimpleMapReduce(object):
+from ./model import PolicyNet, EvaluateNet
+
+class EvolutionManager(object):
     
-    def __init__(self, map_func, reduce_func, num_workers=None):
-        """
-        map_func
-
-          Function to map inputs to intermediate data. Takes as
-          argument one input value and returns a tuple with the key
-          and a value to be reduced.
-        
-        reduce_func
-
-          Function to reduce partitioned version of intermediate data
-          to final output. Takes as argument a key as produced by
-          map_func and a sequence of the values associated with that
-          key.
-         
-        num_workers
-
-          The number of workers to create in the pool. Defaults to the
-          number of CPUs available on the current host.
-        """
-        self.map_func = map_func
-        self.reduce_func = reduce_func
+    def __init__(self, num_workers, num_population):
         self.pool = multiprocessing.Pool(num_workers)
+        self.policy_nets = [PolicyNet(TODO) for i in range(num_workers)]
+        self.evaluate_nets = [EvaluateNet(TODO) for i in range(num_population)]
     
-    def partition(self, mapped_values):
-        """Organize the mapped values by their key.
-        Returns an unsorted sequence of tuples with a key and a sequence of values.
-        """
-        partitioned_data = collections.defaultdict(list)
-        for key, value in mapped_values:
-            partitioned_data[key].append(value)
-        return partitioned_data.items()
-    
+    def generation_forward(self):
+        pass
+
+    def mutate(self):
+        pass
+
     def __call__(self, inputs, chunksize=1):
-        """Process the inputs through the map and reduce functions given.
+        """Process the inputs.
         
         inputs
-          An iterable containing the input data to be processed.
+          models/env_name/epochs.
         
         chunksize=1
-          The portion of the input data to hand to each worker.  This
-          can be used to tune performance during the mapping phase.
+          The portion of the input data to hand to each worker.
         """
-        map_responses = self.pool.map(self.map_func, inputs, chunksize=chunksize)
-        partitioned_data = self.partition(itertools.chain(*map_responses))
-        reduced_values = self.pool.map(self.reduce_func, partitioned_data)
-        return reduced_values
+        # rewards should contain the individual_name model_index and rewards
+        step_results = self.pool.map(self.generation_forward, inputs, chunksize=chunksize)
+        # Rank the resualt
+        results = rank(results)
+        mutate_results = self.pool.map(self.mutate, results)
+
+def main(args):
+    em = EvolutionManager()
+    em()
+
+if __name__ == "__main__":
+    # Parse argument.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-w', '--step_per_generation step', type=int, help="TODO", default=1000)
+    parser.add_argument('-n', '--worker', type=int, help='number of cores to use', default=4)
+    parser.add_argument('-g', '--generation_size', type=int, help='generation of evolution', default=100)
+    parser.add_argument('-g', '--population_size', type=int, help='generation of population', default=8)
+    args = parser.parse_args()
+    main(args)
+    
