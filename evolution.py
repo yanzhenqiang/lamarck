16ea378a42d8ed1a08704b38f72c21cc1d575e46
yanzhenqiang <yanzhenqiang@didiglobal.com>
2020-07-02 17:37:08 +0800

    tmp

--- evolution.py
+++ evolution.py
@@ -12,24 +12,22 @@ def es_params(self):
                                     self.state_dict().values())]
 
 def perturb_model(args, model, random_seed, env):
-    new_model = ES(env.observation_space.shape[0],
-                   env.action_space, args.small_net)
     new_model.load_state_dict(model.state_dict())
-    np.random.seed(random_seed)
-    for (k, v) in new_model.es_params():
-        eps = np.random.normal(0, 1, v.size())
-        v += torch.from_numpy(args.sigma*eps).float()
-    return new_model
 
-def evolution_net_mutate(self):
-    pass
+def evolution_net_mutate(self, model, perturb):
+    # TODO:seed
+    if perturb:
+        np.random.seed(1)
+        for v in model.state_dict().values():
+            eps = np.random.normal(0, 1, v.size())
+            v += torch.from_numpy(0.05*eps).float()
 
 def v_wrap(np_array, dtype=np.float32):
     if np_array.dtype != dtype:
         np_array = np_array.astype(dtype)
     return torch.from_numpy(np_array)
 
-def generation_forward(policy_net, evolution_net, env, steps):
+def generation_forward(policy_net, evolution_net, env_name, steps):
     env = gym.make(env).unwrapped
     opt = torch.optim.Adam(policy_net.parameters(), lr=1e-4, betas=(0.9, 0.999))
     gamma = 0.9
@@ -76,29 +74,39 @@ def generation_forward(policy_net, evolution_net, env, steps):
 class EvolutionManager(object):
     
     def __init__(self, args):
+        self.args = args
         self.pool = multiprocessing.Pool(args.worker)
         env = gym.make(args.env)
         o_dim = env.observation_space.shape[0]
         s_dim = 128
         a_dim = env.action_space.n
-        self.nets = []
+
+        self.policy_nets = []
+        self.evaluate_nets = []
+        self.env_names = []
+        self.steps = []
+
+        self.perturb = []
         for i in range(args.poputation):
             policy_net = PolicyNet(o_dim, s_dim, a_dim)
             policy_net.share_memory()
-            self.nets.append(policy_net)
+            self.policy_nets.append(policy_net)
             evaluate_net = EvaluateNet(o_dim, s_dim, a_dim)
             evaluate_net.share_memory()
-            self.nets.append(evaluate_net)
+            self.evaluate_nets.append(evaluate_net)
+            self.env_names.append(self.args.env_name)
+            self.steps.append(self.args.step_per_generation)
 
     def __call__(self, generation_forward_fn, evolution_net_mutate, chunksize=1):
         # rewards should contain the individual_name model_index and rewards
-        # TODO
-        policy_net, evolution_net, env, steps
-        step_results = self.pool.map(generation_forward_fn, inputs, chunksize=chunksize)
-        # Rank the resualt
-        # TODO
-        results = rank(results)
-        mutate_results = self.pool.map(self.mutate, results)
+        for _ in range(self.args.generation):
+            step_results = self.pool.map(generation_forward_fn, self.policy_net, self.evolution_net, self.env_name, self.steps, chunksize=chunksize)
+            # Rank the resualt
+            # results = rank(results)
+            for i in range(args.population):
+                if step_result[i]:
+                    self.perturb.append(True)
+            mutate_results = self.pool.map(evolution_net_mutate, results)
 
 def main(args):
     em = EvolutionManager(args)
@@ -106,7 +114,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-e', '--env', type=str, default="CartPole-v0")
+    parser.add_argument('-e', '--env_name', type=str, default="CartPole-v0")
     parser.add_argument('-s', '--step_per_generation', type=int, default=100)
     parser.add_argument('-w', '--worker', type=int, default=4)
     parser.add_argument('-g', '--generation', type=int, default=10)
